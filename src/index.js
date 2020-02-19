f29902293a64388ffca3d722b55457625ed3e1d6
Andrew Waller <andrewhwaller@gmail.com>
2020-02-19 02:41:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ function render() {
   }
 };
 
-var store = createStore(candyReducer);
+var store = createStore(candyReducer());
 
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
