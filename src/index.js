a70e7ed0f337105ff966f6bb818ba68a24e4c43f
Andrew Waller <andrewhwaller@gmail.com>
2020-02-19 02:43:13 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 // write your createStore function here
-
+var store = createStore(candyReducer);
 function createStore(reducer) {
   let state;
 
@@ -35,7 +35,5 @@ function render() {
     throw new Error("the store's state has not been defined yet")
   }
 };
-
-var store = createStore(candyReducer);
 // use your createStore function and the functions provided here to create a store
 // once the store is created, call an initial dispatch
